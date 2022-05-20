89413e3823ff1eb6b2d0796625dcd68560ae5bce
= <=>
2022-05-20 19:50:09 +0200

    dbh handler bug removed.

--- Homeworks/Kubernetes/train_model_app/config.py
+++ Homeworks/Kubernetes/train_model_app/config.py
@@ -1,10 +1,10 @@
 import os
 
-# os.environ['USER'] = 'root'
-# os.environ['PASSWORD'] = 'fika'
-# os.environ['DB_NAME'] = 'ml'
-# os.environ['PORT'] = '30008'
-# os.environ['HOST'] = 'k8s-master.unic.kg.ac.rs'
+os.environ['USER'] = 'root'
+os.environ['PASSWORD'] = 'fika'
+os.environ['DB_NAME'] = 'ml'
+os.environ['PORT'] = '30008'
+os.environ['HOST'] = 'k8s-master.unic.kg.ac.rs'
 
 user = os.environ['USER']
 password = os.environ['PASSWORD']
--- Homeworks/Kubernetes/train_model_app/database.py
+++ Homeworks/Kubernetes/train_model_app/database.py
@@ -4,10 +4,10 @@ from models.fastAPIModel import ModelAPI
 
 class Db():
     __instance = None
-    _db = None
 
     def __init__(self):
         self._db = mysql.connector.connect(user = config.user, password = config.password, host = config.host, database = config.db_name, port = config.port)
+        self._dbh = self._db.cursor()
         self._create_table()
         Db.__instance = self
 
@@ -19,10 +19,10 @@ class Db():
     
     def get_all_models(self):
         sql = "select * from models"
-        dbh = self._db.cursor()
-        dbh.execute(sql)
-        records = dbh.fetchall()
-
+        
+        self._dbh.execute(sql)
+        records = self._dbh.fetchall()
+        
         models = []
         for row in records:
             path = row[5]
@@ -33,20 +33,20 @@ class Db():
 
     def _create_table(self):
         sql = "create table if not exists models (model_id INT auto_increment NOT NULL, model_name varchar(100) NOT NULL, mse FLOAT NOT NULL, auc_score FLOAT NOT NULL, accuracy FLOAT NOT NULL, model_path varchar(100) NOT NULL, CONSTRAINT NewTable_PK PRIMARY KEY (model_id))"
-        dbh = self._db.cursor()
-        dbh.execute(sql)
+        
+        self._dbh.execute(sql)
         self._db.commit()
 
     def add_model(self, model_name, mse, auc_score, accuracy, model_path):
         sql = "insert into models(model_name, mse, auc_score, accuracy, model_path) values('" +model_name +"', " +str(mse) +", " +str(auc_score) +", " +str(accuracy) +", '" +model_path +"')"
-        dbh = self._db.cursor()
-        dbh.execute(sql)
+        
+        self._dbh.execute(sql)
         self._db.commit()
     
     def get_model_path_by_model_id(self, model_id):
         sql = "select model_path from models where model_id = " +str(model_id)
-        dbh = self._db.cursor()
-        dbh.execute(sql)
-        record = dbh.fetchall()
+        
+        self._dbh.execute(sql)
+        record = self._dbh.fetchall()
         
         return record[0][0]
